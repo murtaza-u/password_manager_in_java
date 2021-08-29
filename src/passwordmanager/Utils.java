b98c7b53f7b1027754b5377da6d2f96d85c93a95
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2021-08-29 23:22:17 +0530

    defined home directory for win,osx,*nix

--- src/passwordmanager/Utils.java
+++ src/passwordmanager/Utils.java
@@ -3,9 +3,17 @@ package src.passwordmanager;
 import java.io.File;
 
 public class Utils {
-    private static String home = "/home/murtaza/.local/share/passwordmanager";
+    private static String home;
 
     Utils() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")){
+            home = System.getProperty("user.home") + "/AppData/passwordmanager/";
+        }
+        else if (os.contains("osx") || os.contains("nix") || os.contains("aix") || os.contains("nux")){
+            home = System.getProperty("user.home") + "/.local/share/passwordmanager/";
+        }
+
         File homeDir = new File(home);
         if (!homeDir.exists()){
             homeDir.mkdirs();
