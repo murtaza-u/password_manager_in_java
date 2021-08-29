b3ad99849193e6da911401dbb1ddf34dd2d3151a
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2021-08-29 23:21:11 +0530

    create password manager's home directory if it doesn't exists

--- src/passwordmanager/Utils.java
+++ src/passwordmanager/Utils.java
@@ -5,6 +5,13 @@ import java.io.File;
 public class Utils {
     private static String home = "/home/murtaza/.local/share/passwordmanager";
 
+    Utils() {
+        File homeDir = new File(home);
+        if (!homeDir.exists()){
+            homeDir.mkdirs();
+        }
+    }
+
     public String[] listFields() {
         File homeDir = new File(home);
         return homeDir.list();
