f6f2644d6be2c023aa95cf9eb2fec8929e8fd24f
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2021-09-16 17:25:34 +0530

    Updated external script

--- extras/script.sh
+++ extras/script.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-list="$(java 2>/dev/null -jar passwordmanager.jar ls)"
-selected="$(echo "$list" | dmenu -i)"
-java 2>/dev/null -jar passwordmanager.jar ls -gui "$selected" | xclip -selection clipboard
+list="$(java 2>/dev/null -jar passwordmanager.jar ls | dmenu -i)"
+java 2>/dev/null -jar passwordmanager.jar ls -gui "$field" | xclip -selection clipboard
